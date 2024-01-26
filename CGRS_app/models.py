29111a6eb517ee2db0cd4c420b7d5abf546f8e54
ARUN M <arun.m@msds.christuniversity.in>
2024-01-26 15:39:08 +0530

    update

--- CGRS_app/models.py
+++ CGRS_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class (models.Model):
+class provider(models.Model):
     pid=models.IntegerField(primary_key=True)
     pname = models.CharField(max_length=30)
     pcontactno = models.CharField(max_length=12)
     pemail=models.CharField(max_length=40)
-    sid=models.ForeignKey(service,on_delete=models.CASCADE)
+    sid=models.ForeignKey(on_delete=models.CASCADE)
     pstatus=models.BooleanField()
     
     def _str_(self):
