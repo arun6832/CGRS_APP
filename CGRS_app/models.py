49a5f500e49b180343f0a1fc7ad142f63a22bd42
ARUN M <arun.m@msds.christuniversity.in>
2024-01-29 12:02:05 +0530

    update

--- CGRS_app/models.py
+++ CGRS_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class user_details(models.Model):
+class user_register(models.Model):
     Register_No=models.ForeignKey(User,on_delete=models.CASCADE,default=0)
     email=models.CharField(max_length=100,null=False)
     password=models.CharField(max_length=100,null=False)
     confirm_password=models.CharField(max_length=100,null=True)
 
+class user_login(models.Model):
+      Register_No=Register_No=models.ForeignKey(User,on_delete=models.CASCADE,default=0)
+      password=password=models.CharField(max_length=100,null=False)
 class Meta:
         app_label = 'CGRS_app'
--- CGRS_app/views.py
+++ CGRS_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .models import user_details,User
+from .models import user_register,User,user_login
 
 
 
