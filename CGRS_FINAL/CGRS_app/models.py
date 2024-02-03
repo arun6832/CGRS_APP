ee35f412f15141347cee4ea40ba435bc92f9d697
shivangiscodes <143073166+shivangiscodes@users.noreply.github.com>
2024-02-04 01:11:16 +0530

    Add files via upload

---
+++ CGRS_FINAL/CGRS_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class New_Reg(models.Model):
+        reg_no=models.CharField(max_length=200)
+        email=models.CharField(max_length=100)
+        name=models.CharField(max_length=200)
+        password=models.CharField(max_length=100,null=False)
+        c_password=models.CharField(max_length=100,null=True)
+
+class complaint(models.Model):
+    reg_no = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    complaint = models.CharField(max_length=10000)
+    image = models.ImageField(upload_to='complaint_proof/')
+
+        # def __str__(self):
+        #         return f"{self.}"
